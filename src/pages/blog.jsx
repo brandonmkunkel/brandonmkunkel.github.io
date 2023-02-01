9584831b7003d940d7776a5dba205dbb7a323056
Brandon Kunkel <38381956+brandonmkunkel@users.noreply.github.com>
2023-01-31 20:03:24 -0500

    Add google analytics

---
+++ src/pages/blog.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PageBody from "../components/page_body";
+
+const BlogPage = () => {
+  return (
+    <PageBody>
+      <h1>Blog</h1>
+      <p>This is where blog content goes</p>
+    </PageBody>
+  );
+};
+
+export default AboutPage;
