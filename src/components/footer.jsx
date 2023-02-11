58695095fc70fa83672d4dbb9f44ace588902189
Brandon Kunkel <38381956+brandonmkunkel@users.noreply.github.com>
2023-02-10 22:53:28 -0500

    Start laying out projects page

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import '../App.css';
+import { Link } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import "../App.css";
 
 // stateless footer
 const FooterComponent = () => {
   return (
     <footer className="footer">
-    Copyright © 2023: &nbsp;
-      <Link to='/'>Brandon Kunkel</Link>
+      Copyright © 2023: &nbsp;
+      <Link to="/" component={RouterLink}>
+        Brandon Kunkel
+      </Link>
     </footer>
   );
 };
