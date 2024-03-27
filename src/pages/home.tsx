4405fe11e1e078bfc0ba4f8a584b947a90aeb8cd
Brandon Kunkel <38381956+brandonmkunkel@users.noreply.github.com>
2024-03-27 12:06:05 -0400

    Simplify email contact button

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,6 +8,7 @@ import github from '../assets/github-logo.svg'
 import githubWhite from '../assets/github-logo-white.svg'
 
 import { Link as RouterLink } from 'react-router-dom'
+import { Email } from '@mui/icons-material'
 
 const HomePage = () => {
   const theme = useTheme()
@@ -28,7 +29,7 @@ const HomePage = () => {
           <Typography variant="h2">I'm Brandon Kunkel</Typography>
           <Typography variant="h4">Software Engineer</Typography>
 
-          <Stack direction="row" gap={2} marginTop={3}>
+          <Stack direction="row" gap={3} marginTop={3}>
             <Link
               component={RouterLink}
               to={'https://www.linkedin.com/in/brandon-kunkel/'}
@@ -55,7 +56,9 @@ const HomePage = () => {
               />
             </Link>
 
-            <MailtoButton email="brandonmkunkel@gmail.com"></MailtoButton>
+            <Link component={RouterLink} to={'mailto:brandonmkunkel@gmail.com'}>
+              <Email sx={{ height: '40px', width: '40px' }} />
+            </Link>
           </Stack>
         </Stack>
 
